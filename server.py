6118e77a3da3105786247f7fd1290931cf53ab99
Your Name <you@example.com>
2020-02-05 17:15:38 -0800

    Created html and routes in server py for the first task

--- server.py
+++ server.py
@@ -2,10 +2,12 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
+
 
 
 app = Flask(__name__)
@@ -22,8 +24,20 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
 
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/register_user", method='POST')
+def get_user_info():
+    """Get email and password from users."""
+
+    return render_template("register_user.html")
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
